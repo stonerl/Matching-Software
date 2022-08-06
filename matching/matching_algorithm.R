318de95cad0660899d8e9c11abcfad645c98964a
Toni Förster <toni.foerster@gmail.com>
2022-08-06 13:34:40 +0200

    slight weighting modifications

--- matching/matching_algorithm.R
+++ matching/matching_algorithm.R
@@ -370,7 +370,7 @@ punkte_algorithmus <-
     # Definition der Gewichtungsvariablen: wie viele Punkte gibt welche uebereinstimmung #Default
     
     Studienfach_Gewichtung = c(10) #Punktzahl fuer dasselbe Studienfach #20
-    Fachbereich_Gewichtung = c(8) #Punktzahl fuer denselben Fachbereich #15
+    Fachbereich_Gewichtung = c(7) #Punktzahl fuer denselben Fachbereich #15
     Fakultaet_Gewichtung = c(5) #Punktzahl fuer diesselbe Fakultaet #10
     BergTal_Gewichtung = c(1) #Punktzahl wenn beide am Berg oder im Tal studieren #5
     
@@ -378,7 +378,7 @@ punkte_algorithmus <-
     Hobbykategorie_Gewichtung = c(2) #Hobby aus selber Kategorie (z.Bsp. Sport (nicht:Other))
     
     genaues_Alter_Gewichtung = c(8) # Punktzahl fuer genaue Altersuebereinstimmung #8
-    ungefaehres_Alter_Gewichtung = c(6) #Punktzahl fuer bis zu 3 Jahre Altersunterschied #3
+    ungefaehres_Alter_Gewichtung = c(5) #Punktzahl fuer bis zu 3 Jahre Altersunterschied #3
     
     Geschlecht_Gewichtung = c(5) #Punktzahl fuer dasselbe Geschlecht
     
@@ -392,7 +392,7 @@ punkte_algorithmus <-
     
     Sprache_Gewichtung = c(15) #Punktzahl fuer uebereinstimmende Sprache #5
     
-    Ankunftsdatum_0Wochen_Gewichtung = c(10) #Punktzahl falls der Tuebinger vor oder mit dem Incoming ankommt #20
+    Ankunftsdatum_0Wochen_Gewichtung = c(12) #Punktzahl falls der Tuebinger vor oder mit dem Incoming ankommt #20
     Ankunftsdatum_2Wochen_Gewichtung = c(6) #Punktzahl falls der Tuebinger bis zu 2 Wochen nach dem Incoming ankommt
     Ankunftsdatum_3Wochen_Gewichtung = c(3) #Punktzahl falls der Tuebinger bis zu 3 Wochen nach dem Incoming ankommt
     
